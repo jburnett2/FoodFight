f204fcb6fc8e6c0241eba2d8abfd010dcc955ef2
mmaron96 <31801484+mmaron96@users.noreply.github.com>
2018-02-17 12:41:02 -0600

    Added a simple player script

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+	public int maxHealth = 100;
+	public int playerNum = 0;
+	public int damage = 5;
+	public int health;
+	// Use this for initialization
+	void Start () {
+		health = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision col){
+		if (col.gameObject.tag == "Food") {
+			health -= damage;
+		}
+	}
+}

387eea85a95ea83eec22859b92998c5cb5d87ab8
mmaron96 <31801484+mmaron96@users.noreply.github.com>
2018-02-17 12:11:30 -0600

    Bouncy Tempura Shrimp

---
+++ Assets/Scripts/DespawnFood.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DespawnFood : MonoBehaviour {
+	public float timer = 2;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timer <= 0) {
+			Destroy (this.gameObject);
+		}
+		timer -= Time.deltaTime;
+	}
+}
--- Assets/Scripts/ThrowFood.cs
+++ Assets/Scripts/ThrowFood.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class ThrowFood : MonoBehaviour {
 
 	public GameObject testShrimp;
+	public float force = 10f;
+	public float timerMax = 10;
 
+	private float count = 0f;
 	// Use this for initialization
 	void Start () {
 		
@@ -13,11 +16,15 @@ public class ThrowFood : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown ("Fire1")) {
+		
+		if (Input.GetButton ("Fire1") && count <= 0) {
 			var tmpFood = Instantiate (testShrimp,gameObject.transform);
 			Rigidbody foodRig = tmpFood.GetComponent<Rigidbody> ();
-			foodRig.AddForce (new Vector3 (0, 0, 10));
+			foodRig.AddForce (new Vector3 (force, 0, 0));
+			count = timerMax;
 		}
+		count -= Time.deltaTime;
+		print (count);
 	}
 
 
